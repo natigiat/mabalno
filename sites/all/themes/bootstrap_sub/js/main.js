018a2deb00eba22363b9da33c5b288ad0d37f089
natigiat <natigiat@gmail.com>
2016-03-16 14:44:24 +0200

    adding cars

--- sites/all/themes/bootstrap_sub/js/main.js
+++ sites/all/themes/bootstrap_sub/js/main.js
@@ -3,9 +3,8 @@ Drupal.behaviors.Main = {
 		
         if (jQuery(".html")[0]){
 
-        	jQuery(".navbar-toggle").click(function(event) {
+        	jQuery(".navbar-toggle").bind('touchstart click', function(){
         		jQuery(".main-nav").toggleClass('mobil-nav-view');
-        		alert(22);
         		return false;
         	});
 
